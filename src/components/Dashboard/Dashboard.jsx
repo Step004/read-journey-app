25fbdaf10f840b8620f54de15bd1473d7ccef234
Step004 <ctepan.filep9@gmail.com>
2024-11-08 00:28:17 +0200

    додав розмітку для десктопу для dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -3,8 +3,10 @@ import Description from "../Description/Description.jsx";
 import Filters from "../Filters/Filters.jsx";
 import Quote from "../Quote/Quote.jsx";
 import css from "./Dashboard.module.css";
+// import { useLocation } from "react-router-dom";
 export default function Dashboard() {
   const isLargeScreen = useMediaQuery({ query: "(min-width: 1440px)" });
+  // const location = useLocation().pathname == "./library";
 
   return (
     <section className={css.dashboardSection}>
--- src/pages/MyLibraryPage/MyLibraryPage.jsx
+++ src/pages/MyLibraryPage/MyLibraryPage.jsx
@@ -1,13 +1,18 @@
 import { Helmet } from "react-helmet-async";
 import AppBar from "../../components/AppBar/AppBar.jsx";
+import css from './MyLibraryPage.module.css'
+import Dashboard from "../../components/Dashboard/Dashboard.jsx";
 
 export default function MyLibraryPage() {
-    return (
-      <main>
-        <Helmet>
-          <title>My Library Page</title>
-        </Helmet>
-        <AppBar />
-      </main>
-    );
+  return (
+    <main className={css.container}>
+      <Helmet>
+        <title>My Library Page</title>
+      </Helmet>
+      <AppBar />
+      <div className={css.recommendedAndDashboard}>
+        <Dashboard />
+      </div>
+    </main>
+  );
 }
